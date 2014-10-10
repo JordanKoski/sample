23c6eb1f3b19625770d01f7dd50b5fa83e6034a4
JordanKoski <jordan_koski@asl.oeg>
2014-10-10 10:54:03 +0100

    Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Hello {
   public static void main(String[] args) {
-    System.out.println("Hello, Github");
-  }
+    Scanner scnr = new Scanner(System.in);
+    System.out.println("Hello");
+    System.out.println("What is your favorite colour");
+    String colour = scnr.next();
+    System.out.println(colour + " is my favourite colour too!");
+    System.out.println(" ");
+    System.out.println("Do you have pet?");
+    String pet = scnr.next();
+    System.out.println("I have a " + pet + " too!");
+    
+
+   
+    }
 }
